46fdd9e46512c6ef90fd42f743b23e04efda601d
Alex Artemov <Alex Artemov>
2016-08-21 23:37:17 +0300

    first commit

---
+++ SimpleCalc/SimpleCalc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lohika.ADM.AAP
+{
+    public class SimpleCalc
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Mult(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ UnitTestProject1/SimpleCalcBasicTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace lohika.ADM.AAP
+{
+    [TestFixture]
+    public class SimpleCalcBasicTests
+    {
+        [Test]
+        public void Test_Add2and2_Expected4()
+        {
+            var res = SimpleCalc.Add(2, 2);
+            Assert.True(4 == res);
+        }
+
+        [Test]
+        [Ignore("Not implemented")]
+        public void Test_Sub4and4_Expected0()
+        {
+            throw new NotImplementedException("Not implemented yet...");
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void Test_Mult5and2_Expected10()
+        {
+            var res = SimpleCalc.Mult(5, 2);
+            Assert.AreEqual(10, res, 0);
+        }    
+        
+        [Test]    
+        public void test_Div10and2_Expected5()
+        {
+            var res = SimpleCalc.Div(10, 2);
+            Assert.True(4 == res);
+        }
+    }
+}
