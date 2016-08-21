cc92828bdeea29fad257aa8682b4965dc85bb96e
Alex Artemov <Alex Artemov>
2016-08-22 00:22:44 +0300

    param test changed

--- UnitTestProject1/SimpleCalcParameterizedTests.cs
+++ UnitTestProject1/SimpleCalcParameterizedTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace lohika.ADM.AAP
 {
@@ -33,5 +35,25 @@ namespace lohika.ADM.AAP
             Assert.AreEqual(res, SimpleCalc.Div(a, b));
         }
         
+        public class MultData
+        {
+            public static IEnumerable Data {
+                get {
+                    var TestCases = new List<TestCaseData>();
+                    TestCases.Add(new TestCaseData(2, 2, 4).SetName("Test 2 * 4; Expected:  4"));
+                    TestCases.Add(new TestCaseData(2, 0, 0).SetName(" 2 * 0 = 0").Ignore("Ignore this case!"));
+                    TestCases.Add(new TestCaseData(10, 10, 100).SetName(" 10 * 10 = 100"));
+
+                    return TestCases;                                   
+                }
+            }
+        }     
+
+        [Test]
+        [TestCaseSource(typeof(MultData), "Data")]
+        public void Test_Mult(double a, double b, double res)
+        {
+            Assert.True(res == SimpleCalc.Mult(a, b));
+        }
     }
 }
